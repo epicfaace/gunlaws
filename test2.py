d20de27b3088fe5120560f52d367a1133d6c888f
Ashwin Ramaswami <aramaswamis@gmail.com>
2023-04-24 19:46:12 +0000

    commit

--- test2.py
+++ test2.py
@@ -1,5 +1,7 @@
+import concurrent.futures
 import openai
 import json
+import os
 
 def get_response(message):
     response = openai.ChatCompletion.create(
@@ -21,22 +23,28 @@ with open("output.json", "r") as f:
             chunks[-1] += "\n\n" + txt
             if len(chunks[-1]) > 6000:
                 chunks.append("")
+print("Chunks", len(chunks))
 
 with open("lines.txt", "w+") as f:
     f.write("\n".join(chunks))
 
 results = []
-def run(i):
-    for chunk in chunks:
-        prompt = f"""
-            I need to find historical gun laws analogous. From the list of historical gun laws below, please select the ones that are most similar to: 0) private property restrictions, 1) restrictions in city-owned buildings, 2) restrictions in federal / state buildings, 3) restrictions in school or child care facilities, 4) restrictions in public parks, 5) restrictions in shelters to protect at-risk people, 6) restrictions in nature preserves, 7) restrictions in voter service centers, 8) restrictions in public transportation, 9) restrictions in liquor establishments, 10) restrictions at public gatherings, 11) restrictions at entertainment establishments, 12) restrictions at cannabis dispensaries, and 13) restrictions at healthcare facilities. If no historical gun law was found analogous, just skip it in your answer. Provide the excerpt of the appropriate gun law in your answer. Include laws only about carrying guns, not about firing or shooting guns.
 
-            Historical gun laws:
-            {chunk}
-        """
-        # print(prompt)
-        response = get_response(prompt)
-        print(response)
-        os.makedirs("output", exist_ok=True)
-        with open(f"output/response-{i}.txt", "w+") as f:
-            f.write(response)
+def run(i, chunk):
+    prompt = f"""
+        I need to find historical gun laws analogous. From the list of historical gun laws below, please select the ones that are most similar to: 0) private property restrictions, 1) restrictions in city-owned buildings, 2) restrictions in federal / state buildings, 3) restrictions in school or child care facilities, 4) restrictions in public parks, 5) restrictions in shelters to protect at-risk people, 6) restrictions in nature preserves, 7) restrictions in voter service centers, 8) restrictions in public transportation, 9) restrictions in liquor establishments, 10) restrictions at public gatherings, 11) restrictions at entertainment establishments, 12) restrictions at cannabis dispensaries, and 13) restrictions at healthcare facilities. If no historical gun law was found analogous, just skip it in your answer. Provide the excerpt of the appropriate gun law in your answer. Include laws only about carrying guns, not about firing or shooting guns.
+
+        Historical gun laws:
+        {chunk}
+    """
+    response = get_response(prompt)
+    print(response)
+    os.makedirs("output", exist_ok=True)
+    with open(f"output/response-{i}.txt", "w+") as f:
+        f.write(response)
+    return response
+
+with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
+    futures = [executor.submit(run, i, chunk) for i, chunk in enumerate(chunks)]
+    for future in concurrent.futures.as_completed(futures):
+        results.append(future.result())
